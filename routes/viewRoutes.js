09bbe9d4572b19162ef60a9600161363389b86ed
amanagayev <59646712+amanagayev@users.noreply.github.com>
2021-06-29 14:20:23 +0300

    Improved stripe implementation

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,7 +6,13 @@ const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', bookingController.createBookingCheckout, authController.isLoggedIn, viewController.getOverview);
+router
+    .get(
+        '/',
+        // bookingController.createBookingCheckout,
+        authController.isLoggedIn,
+        viewController.getOverview
+    );
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForum);
 router.get('/me', authController.protect, viewController.getAccount);
